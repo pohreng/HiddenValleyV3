5d5f7652fac5db7f5e03f74ee7c1ef50a3570cf7
pohreng <56568717+pohreng@users.noreply.github.com>
2020-01-05 22:38:35 +0800

    HI

--- app/src/main/java/com/example/hiddenvalleyv3/MainActivity.kt
+++ app/src/main/java/com/example/hiddenvalleyv3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hiddenvalleyv3
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -61,6 +62,24 @@ class  MainActivity : AppCompatActivity() {
                 for(i in 0..(data.size-1)){
                     afterUsername.append(data.get(i).username)
                 }
+
+                    val clickerLetter = findViewById<ImageView>(R.id.letterIMG)
+                    val clickerNumber = findViewById<ImageView>(R.id.numberIMG)
+                    val clickerMatch = findViewById<ImageView>(R.id.matchIMG)
+                    val clickerCal = findViewById<ImageView>(R.id.calculateIMG)
+
+                    clickerLetter.setOnClickListener{
+                        showLetGame()
+                    }
+                    clickerNumber.setOnClickListener{
+                        showNumGame()
+                    }
+                    clickerMatch.setOnClickListener{
+                        showMatchGame()
+                    }
+                    clickerCal.setOnClickListener{
+                        showCalGame()
+                    }
             }
             else {
                 Toast.makeText(this, "Username Or Password Incorrect", Toast.LENGTH_SHORT).show()
@@ -74,7 +93,6 @@ class  MainActivity : AppCompatActivity() {
         login_layout.visibility=View.GONE
         main123.visibility=View.GONE
         afterLogin_layout.visibility=View.GONE
-
     }
     private fun showLogin(){
         registration_layout.visibility=View.GONE
@@ -87,12 +105,39 @@ class  MainActivity : AppCompatActivity() {
         login_layout.visibility=View.GONE
         main123.visibility=View.VISIBLE
         afterLogin_layout.visibility=View.GONE
-
     }
     private fun afterLoginPages(){
         registration_layout.visibility=View.GONE
         login_layout.visibility=View.GONE
         main123.visibility=View.GONE
         afterLogin_layout.visibility=View.VISIBLE
+    }
+    private fun showNumGame(){
+        number_game.visibility=View.VISIBLE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+    }
+    private fun showLetGame(){
+        letter_game.visibility=View.VISIBLE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+    }
+    private fun showMatchGame(){
+        match_game.visibility=View.VISIBLE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+    }
+    private fun showCalGame(){
+        cal_game.visibility=View.VISIBLE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
     }
 }
